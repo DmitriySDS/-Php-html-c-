802f8974bd4c6f4d275e6fdfb4c25c85c4fd2e24
DmitriySDS <44865722+DmitriySDS@users.noreply.github.com>
2018-11-09 09:59:01 +1000

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	
+
+
+<form action="" method="post">
+	<h3 class="a1">ведите имя</h3>
+	<input class ="a1" type="text" name="name">
+	<br><button name="btn" class ="a1">Ok</button>
+	
+</form>
+
+</body>
+</html>
+
+
+<?php echo $_POST['name'];
+
+
+
+
